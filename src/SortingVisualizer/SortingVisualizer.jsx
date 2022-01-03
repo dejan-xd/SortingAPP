58dd1d953562af18b152e7f554720064e2306fc6
dejan-xd <dejan.jovanovic94@gmail.com>
2022-01-03 19:39:55 +0100

    add animations and fix merge sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -11,6 +11,7 @@ export default class SortingVisualizer extends React.Component {
 			array: [],
       height: props.height
 		};
+    this.processing = false;
 	}
 
 	componentDidMount() {
@@ -18,8 +19,15 @@ export default class SortingVisualizer extends React.Component {
 	}
 
 	resetArray() {
+    if (this.processing) return
+
 		const array = [];
     const length = window.innerWidth / 4 - 50;
+
+    const arrayBars = document.getElementsByClassName('array-bar');
+    for(var i = 0; i < arrayBars.length; i++){
+      arrayBars[i].style.backgroundColor = "pink";
+    }
 
 		for (let i = 0; i < length; i++) {
 			array.push(randomIntFromInterval(5, window.innerHeight - 150));
@@ -28,10 +36,39 @@ export default class SortingVisualizer extends React.Component {
 	}
 
   mergeSort() {
-    const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a - b);
-    const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+    this.processing = true;
+    const animations = sortingAlgorithms.mergeSort(this.state.array);
+    const newAnimations = [];
 
-    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+    for (const animation of animations) {
+      newAnimations.push(animation.comparison);
+      newAnimations.push(animation.comparison);
+      newAnimations.push(animation.override);
+    }
+
+    for (let i = 0; i < newAnimations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = newAnimations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? 'yellow' : 'green';
+
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * 3);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = newAnimations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * 3);
+      }
+    }
+    this.processing = false;
   }
 
   quickSort() {}
