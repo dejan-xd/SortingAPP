9abd0cd95ce7f9d8f5212e31102b4829b00c6df7
dejan-xd <dejan.jovanovic94@gmail.com>
2022-02-01 19:42:19 +0100

    refactoring the code

--- src/SortingAlgorithms/BubbleSort.js
+++ src/SortingAlgorithms/BubbleSort.js
@@ -1,6 +1,5 @@
 import { swap } from '../Utils/Utils.js';
 
-
 export function bubbleSort(array) {
     if (array.length <= 1) return array;
 
@@ -13,12 +12,15 @@ export function bubbleSort(array) {
     while (!sorted) {
         sorted = true;
         for (let i = 0; i < auxiliaryArray.length - 1 - round; i++) {
+            // while passing through the list color pillars to yellow and the pillar which is in focus to red
             animations.push([i, i + 1, "passing"]);
             if (auxiliaryArray[i] > auxiliaryArray[i + 1]) {
+                // same colloring but this time with swapping
                 animations.push([i, i + 1, "swap"]);
                 swap(auxiliaryArray, i, i + 1);
                 sorted = false;
             }
+            // collor the pillars to yellow
             animations.push([i, i + 1, "other"]);
         }
         round++;
